9a291fc80ea0835784c04ab9f87ce30e99cf8445
Gazi Akter <gaziakterdev@gmail.com>
2024-10-16 21:12:04 +0600

    category model function

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,9 +2,22 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['name'];
+
+    public function subcategories()
+    {
+        return $this->hasMany(Subcategory::class);
+    }
+
+    public function books()
+    {
+        return $this->hasManyThrough(Book::class, Subcategory::class);
+    }
 }

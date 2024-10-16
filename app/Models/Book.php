18d6f849865cdb3d25b54f8764bf058ed8bcde6e
Gazi Akter <gaziakterdev@gmail.com>
2024-10-16 21:11:15 +0600

    book model function

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,9 +2,22 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['title', 'author', 'description', 'published_at'];
+
+    public function subcategories()
+    {
+        return $this->belongsToMany(Subcategory::class, 'book_subcategory');
+    }
+
+    public function categories()
+    {
+        return $this->hasManyThrough(Category::class, Subcategory::class, 'id', 'id', 'subcategory_id', 'category_id');
+    }
 }

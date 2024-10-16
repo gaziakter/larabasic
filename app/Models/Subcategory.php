4d7b9e82614182eadc0a62bc60c00fe7f3b34cc4
Gazi Akter <gaziakterdev@gmail.com>
2024-10-16 21:12:44 +0600

    sub category model function

--- app/Models/Subcategory.php
+++ app/Models/Subcategory.php
@@ -2,9 +2,22 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Subcategory extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['name', 'category_id'];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function books()
+    {
+        return $this->belongsToMany(Book::class, 'book_subcategory');
+    }
 }
